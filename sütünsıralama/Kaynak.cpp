c22beb8128f42e036d8ef6e2f6f3809e0bb6e0fb
Dogukan Muslukcu <202003011008@dogus.edu.tr>
2021-01-22 15:03:21 +0300

    Add project files.

---
+++ sütünsıralama/Kaynak.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main() {
+	void satýrSýralama(int x[][5], int m, int n);
+	int x[2][5] = { {-1,-2,16,9,-4},{8,-2,17,41,9} }; // pattern matris for sorting
+	
+	satýrSýralama(x, 2, 5);
+	for (int i = 0; i < 2; i++) 
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cout << x[i][j] << "	";
+		}
+		cout << endl;
+	}
+}
+
+
+void satýrSýralama(int x[][5],int m , int n) {// line sort function
+	int t;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n-1; j++) {
+			for (int k = j + 1; k < n; k++) {
+			if(x[i][j]>x[i][k]){
+				t = x[i][j];
+				x[i][j] = x[i][k];
+				 x[i][k] = t;
+		     	}
+			}
+		}
+	}
+}
+void sutunSýralama(int x[][4],int m , int n) {// column sort function
+	int t;
+	for (int j = 0; j < n; j++) {
+		for (int i = 0; i < m - 1; i ++) {
+			for (int k = i + 1; k < m; k++) {
+				if (x[i][j]>x[k][j]) {
+					t = x[i][j];
+					x[i][j] = x[k][j];
+					x[k][j] = t;
+				}
+				
+			}
+		}
+	}
+}
